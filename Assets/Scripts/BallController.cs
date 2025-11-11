2ccca5a55f7aa7384caae94fce4cafea47a87e1f
nandinih02 <79862681+nandinih02@users.noreply.github.com>
2025-11-11 02:06:04 -0500

    Added Platform Raycast Check

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
     [SerializeField]
     private float speed;
     private bool started;
+    private bool gameOver;
     public InputAction directionChange;
 
     Rigidbody rb;
@@ -29,6 +30,7 @@ public class BallController : MonoBehaviour
     void Start()
     {
         started = false;
+        gameOver = false;
 
     }
 
@@ -43,11 +45,19 @@ public class BallController : MonoBehaviour
                 started = true;
             }
         }
+
+        if(! Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+            //rb.linearVelocity=new Vector3(0f, -25f, 0f);
+
+            Debug.Log("Game Over");
+        }
         
         if(directionChange.triggered)
-            {
-                SwitchDirection();
-            }
+        {
+            SwitchDirection();
+        }
         
 
     }
