8b1f9b8c471fbc4ec4464d607be8a67ed001690e
nandinih02 <79862681+nandinih02@users.noreply.github.com>
2025-11-26 02:32:20 -0500

    Added UI Manager controls

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,11 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager instance;
+    public GameObject zigzagPanel;
+    public GameObject gameOverPanel;
+    public TMP_Text taptext;
+    public TMP_Text highScore1;
+    public TMP_Text highScore2;
+    public Button reset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if(instance==null)
+        {
+            instance=this;
+        }
+    }
     void Start()
     {
-        
+        gameOverPanel.SetActive(false);
+        zigzagPanel.SetActive(true);
+    }
+
+    public void GameStart()
+    {
+        taptext.enabled = false;
+        zigzagPanel.GetComponent<Animator>().Play("panelUp");
+
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
     }
 
     // Update is called once per frame
